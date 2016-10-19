45dcd29abdf98220bcb89936389e8956cf902316
vijayku86 <vijay8610yadav@gmail.com>
2016-10-19 14:38:44 +0530

    Added Tab Bar demo
    Committing all files

---
+++ TabBarExample/TabBarExample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabBarExample
+//
+//  Created by NIIT Technologies on 7/9/16.
+//  Copyright © 2016 NIIT Technologies. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
